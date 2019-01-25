610883753af41cb0bcfc041a23753dccf86d9c9c
cawcaw <dmitrygrach@gmail.com>
2019-01-26 02:59:40 +0300

    Render texture

--- shader/simple_150.glslf
+++ shader/simple_150.glslf
@@ -1,9 +1,17 @@
 #version 150 core
 
-in vec4 v_Color;
-out vec4 Target0;
+uniform sampler2D t_Dif;
+
+in vec2 v_Uv;
+out vec4 o_Color;
 
 void main()
 {
-    Target0 = v_Color;
+  vec3 dif = vec3(texture(t_Dif, v_Uv).r);
+
+  vec3 result = vec3(0.0);
+  // manipulations here
+  result = dif;
+
+  o_Color = vec4(dif, 1.0);
 }
--- src/main.rs
+++ src/main.rs
@@ -13,42 +13,50 @@ use gfx_core::format::{DepthStencil, Rgba8, Srgba8};
 
 gfx_vertex_struct!( Vertex {
     pos: [f32; 2] = "a_Pos",
-    color: [f32; 2] = "a_Color",
+    uv: [f32; 2] = "a_Uv",
 });
 
 impl Vertex {
-    fn new(p: [f32; 2], c: [f32; 2]) -> Vertex {
+    fn new(p: [f32; 2], u: [f32; 2]) -> Vertex {
         Vertex {
             pos: p,
-            color: c,
+            uv: u,
         }
     }
 }
 
 gfx_pipeline!( pipe {
     vbuf: gfx::VertexBuffer<Vertex> = (),
+    dif: gfx::TextureSampler<[f32; 4]> = "t_Dif",
     out: gfx::RenderTarget<Srgba8> = "Target0",
 });
 
 const CLEAR_COLOR: [f32; 4] = [0.02, 0.02, 0.02, 1.0];
 
+fn load_texture<R, F>(factory: &mut F, data: &[u8])
+                -> Result<gfx::handle::ShaderResourceView<R, [f32; 4]>, String> where
+                R: gfx::Resources, F: gfx::Factory<R> {
+    use std::io::Cursor;
+    use gfx::texture as t;
+    let img = image::load(Cursor::new(data), image::PNG).unwrap().to_rgba();
+    let (width, height) = img.dimensions();
+    let kind = t::Kind::D2(width as t::Size, height as t::Size, t::AaMode::Single);
+    let (_, view) = factory.create_texture_immutable_u8::<Rgba8>(kind, t::Mipmap::Provided, &[&img]).unwrap();
+    Ok(view)
+}
+
 fn main()
 {
     let mut events_loop = glutin::EventsLoop::new();
-
     let window_size = glutin::dpi::LogicalSize::new(768.0, 768.0);
-
     let window_builder = glutin::WindowBuilder::new()
         .with_title("Darkness")
         .with_dimensions(window_size);
-
     let context = glutin::ContextBuilder::new();
-
     let (window, mut device, mut factory, rtv, _stv) =
         gfx_window_glutin::init::<Srgba8, DepthStencil>(window_builder, context, &events_loop)
         .expect("Failed!");
     let mut encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
-
     let pso = factory.create_pipeline_simple(
         include_bytes!("../shader/simple_150.glslv"),
         include_bytes!("../shader/simple_150.glslf"),
@@ -65,15 +73,16 @@ fn main()
         Vertex::new([-0.4,  0.4], [0.0, 0.0]),
     ];
 
+    let dif_texture = load_texture(&mut factory, &include_bytes!("../res/concrete.png")[..]).unwrap();
     let (vertex_buffer, slice) = factory.create_vertex_buffer_with_slice(&vertex_data, ());
-
+    let sampler = factory.create_sampler_linear();
     let data = pipe::Data {
         vbuf: vertex_buffer,
+        dif: (dif_texture, sampler),
         out: rtv
     };
 
     let mut running = true;
-
     while running
     {
         events_loop.poll_events(|event| {
@@ -99,7 +108,7 @@ fn main()
         encoder.draw(&slice, &pso, &data);
         encoder.flush(&mut device);
         window.swap_buffers().unwrap();
-        println!("{:?}", time::now());
+        // println!("{:?}", time::now());
         device.cleanup();
     }
 }
