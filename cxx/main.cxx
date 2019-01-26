5dcbe9c21947a05935e63a9ed13c194aa3d81f78
cawcaw <dmitrygrach@gmail.com>
2019-01-27 01:54:01 +0300

    Add C++ base

---
+++ cxx/main.cxx
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// #include <GLFW/glfw3.h>
+
+int main(int argc, char ** argv)
+{
+  printf("darkness started\n");
+
+  // main loop
+
+  printf("darkness stoped\n");
+  return 0;
+}
